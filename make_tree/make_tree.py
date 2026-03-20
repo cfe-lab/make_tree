2d0782f299cdd2568762dab720f5a499a6ee4481
Vitaliy Mysak <vmysak@bccfe.ca>
2026-03-20 16:29:01 +0000

    Remove command-line interface from the tree maker script

--- make_tree/make_tree.py
+++ make_tree/make_tree.py
@@ -221,29 +221,3 @@ def load_tree(input_path: str) -> ete3.Tree:
     reverse_tree(t)  # TODO: make sure this is still required.
     process_tree_labels(t)
     return t
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Tree maker script. Produces pretty PDFs."
-    )
-    parser.add_argument(
-        "input_path",
-        help="Input (.ph) file path. Expected to have the input tree definition. Newick format is supported",
-    )
-    parser.add_argument("output_path", help="Output (.pdf) file path")
-    parser.add_argument(
-        "title", nargs=argparse.OPTIONAL, default=None, help="Tree title"
-    )
-    args = parser.parse_args()
-
-    try:
-        t = load_tree(args.input_path)
-    except ete3.parser.newick.NewickError as e:
-        print(
-            "Error: The tree file is not in the expected format. Please make sure it uses the standard Newick format.",
-            file=sys.stderr,
-        )
-        exit(1)
-
-    export_tree(t, args.output_path, args.title)
